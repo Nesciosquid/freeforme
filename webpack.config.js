efdbf7c2553fd9f589d4c2dfee939a15a49fc386
Aaron Heuckroth <a.heuckroth@gmail.com>
2016-07-04 14:14:25 -0400

    try new sync script.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ var path = require('path');
 var config = {
   entry: path.resolve(__dirname, 'src/app.js'),
   output: {
-    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js'
   },
   module: {
